682be30eead5b3f68a093a3a85ec187d75a0a99f
Aitor <aitorpalomares@hotmail.com>
2021-02-25 18:54:57 +0000

    Updates

--- src/components/styled-components/__tests__/Task.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Task } from '@components/styled-components/Task';
-import 'jest-styled-components';
-
-it('renders correctly', () => {
-  const { container } = render(<Task />);
-  expect(container).toMatchSnapshot();
-});
