c42bdce1d979da49003254c74a15a4237d2cc3f8
Aitor <aitorpalomares@hotmail.com>
2021-02-25 23:23:30 +0000

    add button type

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import Wrapper from './Button.styled';
 
-export const Button = ({ children, label, onClick, disabled, outline }) => {
+export const Button = ({
+  children,
+  label,
+  onClick,
+  disabled,
+  outline,
+  type = 'button',
+}) => {
   const content = children || label;
   const handleClick = () => {
     if (!disabled && onClick) {
@@ -10,7 +17,7 @@ export const Button = ({ children, label, onClick, disabled, outline }) => {
   };
 
   return content ? (
-    <Wrapper outline={outline} onClick={handleClick}>
+    <Wrapper outline={outline} onClick={handleClick} type={type}>
       {content}
     </Wrapper>
   ) : null;
